be36662ca88418b13ce6e97e0f69349e82325120
AzuRin <asrin.rwid@gmail.com>
2023-08-20 11:56:41 +0800

    Finish realtime data extraction from bmkg.go.id

---
+++ gempa_terkini/__init__.py
@@ -0,0 +1,77 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def data_extraction():
+    """
+    Tanggal: 19 Agustus 2023
+    Waktu: 01:29:15 WIB
+    Magnitudo: 5.0
+    Kedalaman: 10 km
+    Lokasi: 8.99 LS - 111.06 BT
+    Keterangan: Pusat gempa berada di laut 90 km Barat Daya Pacitan
+    Dirasakan: III Pacitan, II Gunungkidul, II Bantul, II Yogyakarta
+    :return:
+    """
+    try:
+        content = requests.get('https://bmkg.go.id')
+    except Exception:
+        return None
+    if content.status_code == 200:
+        soup = BeautifulSoup(content.text, 'html.parser')
+
+        result = soup.find('span', {'class': 'waktu'})
+        result = result.text.split(', ')
+        tanggal = result[0]
+        waktu = result[1]
+
+        result = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+        result = result.findChildren('li')
+        i = 0
+        magnitude = None
+        ls = None
+        bt = None
+        lokasi = None
+        dirasakan = None
+
+        for res in result:
+            print(i, res)
+            if i == 1:
+                magnitude = res.text
+            elif i == 2:
+                kedalaman = res.text
+            elif i == 3:
+                koordinat = res.text.split(' - ')
+                ls = koordinat[0]
+                bt = koordinat[1]
+            elif i == 4:
+                lokasi = res.text
+            elif i == 5:
+                dirasakan = res.text
+            i = i + 1
+
+    hasil = dict()
+    hasil['tanggal'] = tanggal
+    hasil['waktu'] = waktu
+    hasil['magnitude'] = magnitude
+    hasil['kedalaman'] = kedalaman
+    hasil['koordinat'] = {'ls': ls, 'bt': bt}
+    hasil['lokasi'] = lokasi
+    hasil['dirasakan'] = dirasakan
+
+    return hasil
+
+
+def display_data(result):
+    if result is None:
+        print("Unable to locate data gempa terkini")
+        return
+
+    print('::Gempa Terakhir Berdasarkan BMKG::\n')
+    print(f"Tanggal\t\t: {result['tanggal']}")
+    print(f"Waktu\t\t: {result['waktu']}")
+    print(f"Magnitude\t: {result['magnitude']}")
+    print(f"Kedalaman\t: {result['kedalaman']}")
+    print(f"Koordinat\t: LS = {result['koordinat']['ls']}, BT = {result['koordinat']['bt']}")
+    print(f"Lokasi\t\t: {result['lokasi']}")
+    print(f"Dirasakan\t: {result['dirasakan']}")
--- main.py
+++ main.py
@@ -1,44 +1,11 @@
 """
 Aplikasi Deteksi Gempa Terkini
 MODULARISASI DENGAN FUNCTION
+MODULARISASI DENGAN PACKAGE
 """
-
-
-def ekstraksi_data():
-    """
-    Tanggal: 15 Agustus 2023
-    Waktu: 10:54:06 WIB
-    Magnitudo: 5.8
-    Kedalaman: 164 km
-    Lokasi: LS = 8.65  BT = 121.51
-    Pusat Gempa: 22 km TimurLaut MBAY-NAGEKEO-NTT
-    Potensi Tsunami: Tidak berpotensi TSUNAMI
-    :return:
-    """
-    hasil = dict()
-    hasil['tanggal'] = '15 Agustus 2023'
-    hasil['waktu'] = '10:54:06 WIB'
-    hasil['magnitudo'] = '5.8'
-    hasil['kedalaman'] = '164 km'
-    hasil['lokasi'] = {'ls': 8.65, 'bt': 121.51}
-    hasil['pusat'] = '22 km TimurLaut MBAY-NAGEKEO-NTT'
-    hasil['tsunami'] = 'Tidak berpotensi TSUNAMI'
-
-    return hasil
-
-
-def tampilkan_data(result):
-    print('Gempa terakhir berdasarkan BMKG')
-    print(f"Tanggal\t\t: {result['tanggal']}")
-    print(f"Waktu\t\t: {result['waktu']}")
-    print(f"Magnitudo\t: {result['magnitudo']}")
-    print(f"Kedalaman\t: {result['kedalaman']}")
-    print(f"Lokasi\t\t: LS = {result['lokasi']['ls']}, BT = {result['lokasi']['bt']}")
-    print(f"Pusat Gempa\t: {result['pusat']}")
-    print(f"Tsunami\t\t: {result['tsunami']}")
-
+import gempa_terkini
 
 if __name__ == "__main__":
-    print("Aplikasi utama")
-    result = ekstraksi_data()
-    tampilkan_data(result)
+    print('Main App')
+    result = gempa_terkini.data_extraction()
+    gempa_terkini.display_data(result)
